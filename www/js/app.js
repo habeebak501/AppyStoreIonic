55228f89fc23e2c871ee13d05b770afc57065aec
habeebak501 <habeebak501@gmail.com>
2016-10-18 19:27:20 +0530

    search

--- www/js/app.js
+++ www/js/app.js
@@ -1,44 +1,49 @@
 /**
-*@FileName:app.js
-*@CreatedBy:Habeeb
-*@Date:
-*@Purpose:Routing for Ionic Application
-*/
+ *@FileName:app.js
+ *@CreatedBy:Habeeb
+ *@Date:
+ *@Purpose:Routing for Ionic Application
+ */
 /*Include all Dependencies*/
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','angular-carousel-3d','ionicImgCache'
-])
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})/*config method is used to routing different states*/
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('category', {
-    url: '/category',
-  templateUrl: 'templates/category.html',
-  controller : 'categoryCtrl'
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'angular-carousel-3d', 'ionicImgCache'])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
-  .state('content',{
-    url:'/content/?pid?cid?content_count?caption',
-    templateUrl:'templates/content.html',
-    controller:'contentCtrl'
-  })
-  .state('video',{
-    url:'/video/?url?pid?cid?content_count',
-    templateUrl:'templates/video.html',
-    controller:'videoCtrl'
-  });
+  /*config method is used to routing different states*/
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('category', {
+        url: '/category',
+        templateUrl: 'templates/category.html',
+        controller: 'categoryCtrl'
+      })
+      .state('content', {
+        url: '/content/?pid?cid?content_count?caption',
+        templateUrl: 'templates/content.html',
+        controller: 'contentCtrl'
+      })
+      .state('video', {
+        url: '/video/?url?pid?cid?content_count',
+        templateUrl: 'templates/video.html',
+        controller: 'videoCtrl'
+      })
+      .state('search',{
+        url:'/search',
+        templateUrl:'templates/search.html',
+        controller:'SearchCtrl'
+      });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/category');
-});
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/category');
+  });
